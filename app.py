bb09b4d1f110f3cd24eea6a0ba3731a98132d7c8
Adam Wermus <wermusam@dukes.jmu.edu>
2024-10-01 21:01:22 -0700

    Bootstrap Intro

--- app.py
+++ app.py
@@ -5,17 +5,22 @@ Date: September 30, 2024
 Initial web app using dash
 """
 # Initial import
-from dash import Dash, html, dash_table, dcc, callback, Output, Input
-import pandas as pd
-import plotly.express as px
+import dash
+from dash import html
 import dash_bootstrap_components as dbc
-import dash_mantine_components as dmc
 
 class App():
 
     def __init__(self):
         # initial dash app
-        self._app = Dash()
+        self._app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+    def create_layout(self):
+        self._app.layout = dbc.Container([dbc.Row(dbc.Col(html.H1('What The Hill',
+        className='text-center'))),
+        dbc.Row(dbc.Col(html.P('A Hill and Stair Exploration Adventure Across Los Angeles.', className='text-center'))),
+        ])
+
 
     def run(self):
         """Runs the application"""
@@ -26,5 +31,6 @@ class App():
 # Run the app
 if __name__ == "__main__":
     app = App()
+    app.create_layout()
     app.run()
 
