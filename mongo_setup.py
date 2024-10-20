4c429f947ef9f8203e7936c42312e4c422e7171d
Adam Wermus <wermusam@dukes.jmu.edu>
2024-10-20 16:51:45 -0700

    successfuly mongodb conneciton in atlas

--- mongo_setup.py
+++ mongo_setup.py
@@ -3,8 +3,9 @@ Author: Adam Wermus
 Date: October 22, 2024
 Mongo DB 
 """
-import pymongo
 import sys
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 # Connection, Database, and Collections
 CONNECTION = "mongodb://localhost:27017/"
@@ -18,13 +19,17 @@ class Mongo():
     def __init__(self):
 
         self._client = pymongo.MongoClient(CONNECTION)
+        "Check if the connection is successful"
+        self._client.admin.command('ping')
         self.db_test = self._client[DB_TEST_NAME]
         # self._db = client[DB_NAME]
         self.collection_test = self.db_test[COLLECTION_NAME]
 
     def insert_submitted_data(self, submission_data):
-        "insert the submitted data into MongoDB"
+        """insert the submitted data into MongoDB"""
+        print("inside insert submitted data")
         self.collection_test.insert_one(submission_data)
+        print("submitted data")
 
     def retrieve_data(self):
         """retrieve data from mongoDB"""
