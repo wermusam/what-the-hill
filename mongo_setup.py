46df25d1c194591c25ffb29fd2db43b66782f443
Adam Wermus <wermusam@dukes.jmu.edu>
2024-10-20 17:17:24 -0700

    successfully submitted data into atlas

--- mongo_setup.py
+++ mongo_setup.py
@@ -3,6 +3,7 @@ Author: Adam Wermus
 Date: October 22, 2024
 Mongo DB 
 """
+import pprint
 import sys
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
@@ -12,24 +13,21 @@ CONNECTION = "mongodb://localhost:27017/"
 DB_NAME = "wth"
 DB_TEST_NAME = "wth_test"
 COLLECTION_NAME = "submissions"
+URI = "mongodb+srv://amwermus:Whatis24!@cluster0.x3myy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 class Mongo():
 
     def __init__(self):
 
-        self._client = pymongo.MongoClient(CONNECTION)
-        "Check if the connection is successful"
-        self._client.admin.command('ping')
+        self._client = MongoClient(URI, server_api=ServerApi('1'))
         self.db_test = self._client[DB_TEST_NAME]
         # self._db = client[DB_NAME]
         self.collection_test = self.db_test[COLLECTION_NAME]
 
     def insert_submitted_data(self, submission_data):
         """insert the submitted data into MongoDB"""
-        print("inside insert submitted data")
         self.collection_test.insert_one(submission_data)
-        print("submitted data")
 
     def retrieve_data(self):
         """retrieve data from mongoDB"""
