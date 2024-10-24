99cae424e6f073fb360d8ef05d27519a5124f0de
Adam Wermus <wermusam@dukes.jmu.edu>
2024-10-23 22:13:58 -0700

    added photo and rules section

--- app.py
+++ app.py
@@ -40,6 +40,19 @@ class Application:
         dropdown_options = self.dropdown_name_options()
         self._app.layout = dbc.Container([
 
+            # Header and Subtitle
+            dbc.Row(dbc.Col(html.H1("Hill Yeah Hillvember!!!"), width={"size": 8, "offset": 2})),
+            dbc.Row(dbc.Col(html.H2("November Project's Hill, Steps, and Steep Street Exploration Challenge Across LA"), width={"size": 8, "offset": 2})),
+
+            # Divider
+            html.Hr(),
+
+            # Rules
+            dbc.Row(dbc.Col(self.paragraph_rules(), width={"size": 8, "offset": 2})),
+
+            # Divider
+            html.Hr(),
+
             # Data Table
             html.H1("What The Hill Locations and Descriptions", style={"textAlign": "center"}),
             self.generate_hill_table(),
@@ -89,7 +102,6 @@ class Application:
         ], style={'width': '100%', 'height': '500px'})
         
 
-
     def dropdown_name_options(self, data=None):
         """Returns the dropdown options from the json data"""
         return [{'label': item["name"], 'value': item['name']} for item in self.hill_data_loader]
@@ -117,6 +129,78 @@ class Application:
         for item in self.hill_data_loader:
             if item["name"] == name:
                 return item["vertical"]
+
+    def image_placer(self):
+        """Used for image in assets dir"""
+        return html.Img(
+            src = "/assets/hill_yeah_img.jpg",
+            style={"width": "100%", "max-width": "300px"}
+        )
+
+    
+    def paragraph_rules(self):
+        # Paragraph explaining the rules
+        return [
+            # Introductory Paragraph
+            html.P("Welcome to November Project's Hill Yeah Challenge! From November 1-November 24, your goals are for you and your team to:"),
+            
+            # Row with two columns: one for the image, one for bullet points
+            dbc.Row([
+
+                # Column for the image
+                dbc.Col(
+                    self.image_placer(), width=4
+                ),
+                # Column for the bullet poitns
+                dbc.Col(
+                    [
+                        # Unordered list of Rules:
+                        html.Ul([
+                        html.Li("Go to as many of the 40 listed locations as you can."),
+                        html.Li("Get as many repetitions for each location as you can."),
+                        html.Li("Get the most vertical feet you can for you and your team."),
+                        ]),
+
+                    ],
+                    width=8 # width ratio for the bullet points colum
+                )
+            ]),
+            
+            # Additional paragraph of rules
+            html.P(
+                "This challenge is inspired by November Project San Francisco's Hill Climb Challenge in 2021." 
+                "Below is a table of all 40 locations showing their names, descriptions, lenght, vertical feet, and a link to their strava segments." 
+                "There is also a map where clicking on the pins will take you to google maps so you know where each location starts."
+                "At the bottom of the page is a form where you will submit your name, e-mail, select the location you did, enter number of reps, and an optional strava link as proof that you completed the route."
+                "When you hit submit, 'RoboAdam' will calculate and keep track of your score for you and the team you are on. You can find that data in the Hill Data Portal."
+            ),
+
+            # Unordered List
+            html.Ul([
+                html.Li("Locations you've completed"),
+                html.Li("Repetitions of each location"),
+                html.Li("Your total vertical feet"),
+            ]),
+
+            # Additional Paragraph
+            html.P(
+                "When you submit your first score, you will be randomly assigned to a team of up to 6 people. If you'd like to be on a team with other people, please let leadership know when you submit and we will try to accomodate you." 
+                "This is an honor system. We trust you. Please don't break that trust." 
+                "If there any issues with the form or data, please contact Adam Wermus at amwermus@gmail.com"
+            ),
+
+            # Unordered List
+                html.Ul([
+                html.Li("Use the map to strategize how to best go to each location"),
+                html.Li("Use the Hill Data Portal by 'RoboAdam'"),
+                html.Li("Support your team. This is fun. Use e-mail, Marco Polo, Whatsapp, anything to support each other to win."),
+            ]),
+
+            # Let's go
+                html.P(
+                    "LET'S GOOOOOOO"
+            )
+        ]
 
     def submission_form_response(self):
         """Handles submission form response, updating data, and visualization"""
